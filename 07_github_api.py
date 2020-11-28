fa74ede7e50438c3988a3b8d74c493816d4101cc
salvador.fuentes <pit.091199@gmail.com>
2020-11-28 13:03:26 -0600

    Homeworks

--- 07_github_api.py
+++ 07_github_api.py
@@ -3,8 +3,8 @@ import requests
 # Constants
 BASE_URL = 'https://api.github.com/'
 
-#Function
-##Getting user info
+#Functions#
+##Getting user info:
 def get_github_user(username):
     url = f'{BASE_URL}users/{username}'
     response = requests.get(url)
@@ -12,8 +12,22 @@ def get_github_user(username):
         return response.json()
     return None
 
+# Para descargar la foto del usuario de github
+def download_github_user_avatar(avatar_url, username):
+    response = requests.get(avatar_url)
+    if response.status_code == 200:
+        #download a file from internet
+        response_content = response.content
+        filename = f'tmp/{username}.png' 
+        with open (filename, 'wb') as image:
+            image.write(response_content)
+            return filename
+    return None
+
 username = input('Give me a Github username: \t')
 user = get_github_user(username)
-print(user)
+filename = download_github_user_avatar(user['avatar_url'], username)
+print(filename)
+
 
 
---
+++ 08_github_users_followers.py
@@ -0,0 +1,4 @@
+#Ocupando los conocimientos de python y requests
+
+# Crear un bot para consultar los followers de un usuario de github.
+# Hecha dicha consulta, descargar los avatars de todos los followers. 
